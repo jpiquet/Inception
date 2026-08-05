f1c36b1a6696a2e623d4a6f1fe719d2bca5d891d
amerzone-exe <jocelyn.piquet1998@gmail.com>
2026-08-05 15:52:03 +0200

    added some modification

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
-# on installe WP-CLI pour gérer WordPress en ligne de commande
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-chmod +x /usr/local/bin/wp
+until mariadb -h mariadb -u root -e "SELECT 1;" >/dev/null 2>&1; do
+    sleep 1
+done
 
 # on crée le fichier de configuration wp-config.php avec les informations de la base de données
-wp config create --allow-root \
-	--dbname=${DB_NAME} \
-	--dbuser=${DB_USER} \
-	--dbpass=${DB_PASSWORD} \
-	--dbhost=mariadb \
-	--path=/var/www/html
+# uniquement si il n'existe pas déjá
+if [ ! -f "/var/www/html/wp-config.php"] then
+	wp config create --allow-root \
+		--dbname=${DB_NAME} \
+		--dbuser=${DB_USER} \
+		--dbpass=${DB_PASSWORD} \
+		--dbhost=mariadb \
+		--path=/var/www/html
+fi
 
 # on installe WordPress avec les informations fournies dans le fichier .env
-wp core install --allow-root \
-	--url=${URL_NAME} \
-	--title=${TITLE_NAME} \
-	--admin_user=${ADMIN_USER} \
-	--admin_password=${ADMIN_PASSWORD} \
-	--admin_email=${ADMIN_EMAIL} \
-	--path=/var/www/html
+if wp core is-installed --allow-root --path=/var/www/html >/dev/null 2>&1; then
+	wp core install --allow-root \
+		--url=${URL_NAME} \
+		--title=${TITLE_NAME} \
+		--admin_user=${ADMIN_USER} \
+		--admin_password=${ADMIN_PASSWORD} \
+		--admin_email=${ADMIN_EMAIL} \
+		--path=/var/www/html
 
-wp user create --allow-root \
+	wp user create --allow-root \
 	${WP_USER} \
 	${WP_EMAIL} \
 	--user_pass=${WP_PASSWORD} \
 	--path=/var/www/html
+fi
 
-
+# on execute php-fpm au premier plan grace a -F (forground)
+exec php-fpm8.4 -F
 
 # Changer le --allow-root car peu etre dangereux, mais pour le moment on le laisse pour que ça fonctionne.
