f6ce25ebc72837d487283e9fc8c4ff3bcd6eb309
amerzone-exe <jocelyn.piquet1998@gmail.com>
2026-08-20 16:27:50 +0200

    pushed script

--- start_script.sh
+++ start_script.sh
@@ -17,11 +17,11 @@ echo "DB_USER=${DBUSER}" >> .env
 
 echo -n "Database user password: "
 read DBPASS
-echo -n "DBPASS" > /secrets/db_passord.txt
+echo -n ${DBPASS} > /secrets/db_passord.txt
 
 echo -n "Database root password: "
 read ROOTPASS
-echo -n "DBPASS" > /secrets/db_root_passord.txt
+echo -n ${ROOTPASS} > /secrets/db_root_passord.txt
 
 # WORDPRESS ENV + SECRETS
 
@@ -34,8 +34,28 @@ do
 	echo -n "Wordpress admin username (can't contains \"admin\"): "
 	read ADMIN
 done
+echo "ADMIN_NAME=${ADMIN}" >> .env
 
 echo -n "Wordpress admin password: "
 read ADMINPASS
+echo -n ${ADMINPASS} > /secrets/admin_password.txt
 
-echo "Choose your "
+echo -n "Wordpress user username: "
+read USER
+echo -n ${USER} >> .env
+
+echo -n "Wordpress guest password: "
+read USERPASS
+echo -n ${USERPASS} > /secrets/user_password.txt
+
+if [ ! -f "/secrets/inception.crt" ]; then
+	openssl req -x509 \
+	-nodes \
+	-days 365 \
+	-newkey rsa:2048 \
+	-out /etc/ssl/certs/inception.crt \
+	-keyout /etc/ssl/private/inception.key \
+	-subj /C=FR/ST=RH/L=Lyon/O=42/OU=42/CN=jpiquet.42.fr
+fi
+
+#rm -f start_script
